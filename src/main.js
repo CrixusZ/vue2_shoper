ec3168df50505937506e2d2a9c1f7cc889496f21
CrixusZ <zengjiahao1989@163.com>
2017-03-27 23:20:14 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import App from './App'
+//import App from './App'
+import Layout from './components/layout'
+import IndexPage from './pages/index'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 
 Vue.use(VueRouter)
 Vue.use(Vuex)
-/*初始化的一个vuex实例*/
 let store = new Vuex.Store({
 	state: {
 		
@@ -24,17 +25,19 @@ let store = new Vuex.Store({
 let router = new VueRouter({
 	mode: 'history',
 	routes : [
-		
+		{
+			path: '/',
+			component: IndexPage
+		}
 	]
 })
-/*编程式的导航，在用户登录前检阅用户信息，如果在未登录状态导入指定的路由*/
-//router.beforEach(router.push())
 new Vue({
-	router,
-	store,
   	el: '#app',
-  	render: h => h(App),
+  	router,
+	store,
+  	template: '<Layout/>',
+  	components: { Layout }
+//	render: h => h(App),
 	//相当于render: function(h){return h(App)}
-	
 })
 
