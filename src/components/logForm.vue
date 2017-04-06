3432c64b7200d97819fbe9b6615151314df78bd2
CrixusZ <zengjiahao1989@163.com>
2017-04-06 09:16:06 +0800

    登录页
    未完成

---
+++ src/components/logForm.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="login-form">
+	    <div class="g-form">
+	      <div class="g-form-line">
+	        <span class="g-form-label">用户名：</span>
+	        <div class="g-form-input">
+	          <input type="text" placeholder="请输入用户名">
+	        </div>
+	        <span class="g-form-error"></span>
+	      </div>
+	      <div class="g-form-line">
+	        <span class="g-form-label">密码：</span>
+	        <div class="g-form-input">
+	          <input type="password" placeholder="请输入密码">
+	        </div>
+	        <span class="g-form-error"></span>
+	      </div>
+	      <div class="g-form-line">
+	        <div class="g-form-btn">
+	          <a class="button">登录</a>
+	        </div>
+	      </div>
+	      <p></p>
+	    </div>
+	</div>
+</template>
+
+<script>
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/regForm.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		regForm
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
